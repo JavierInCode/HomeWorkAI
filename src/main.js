ec8ad8ae381d5a424bdc11b6e7da5d70a1511e61
JavierInCode <vacasjuano43@gmail.com>
2025-04-05 08:06:46 +0200

    add sections

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './main.css';
 
 document.querySelector('#app').innerHTML = `
-  <div class="container">
+  <div class="container" id="main">
     <h1>HomeWork AI</h1>
-    <form>
+    <form id="mainform">
   <label for="curso">Elige Tu Curso:</label>
   <select name="curso" id="curso">
     <option value="4primaria">4° Primaria</option>
@@ -11,11 +11,21 @@ document.querySelector('#app').innerHTML = `
     <option value="6primaria">6° Primaria</option>
     <option value="1eso">1° ESO</option>
   </select>
-      <video id="video" autoplay></video>
   <br><br>
-  <input type="submit" value="Resolver Tarea">
+  <input type="submit" value="Siguiente">
 </form>
+</div>
+  
+  <div class="container" id="camera">
+  <h1>HomeWork AI</h1>
+  <form>
+  <label for="camera">Haz una foto a tu tarea:</label>
+  <video id="video" autoplay></video>
+  <input type="submit" value="Hacer Foto">
+  </form>
+  
   </div>
+  
 `;
 
 navigator.mediaDevices.getUserMedia({
@@ -26,4 +36,24 @@ navigator.mediaDevices.getUserMedia({
 })
 .catch(error => {
   console.error("Could not access the camera:", error);
+});
+
+const x = document.getElementById("camera");
+const y = document.getElementById("main");
+x.style.display = "none";
+
+function showCamera() {
+    x.style.display = "block";
+    y.style.display = "none";
+}
+
+document.getElementById('mainform').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const curso = document.getElementById('curso').value;
+
+  const newUrl = `${window.location.pathname}?curso=${encodeURIComponent(curso)}`;
+  window.history.pushState({}, '', newUrl);
+
+  showCamera();
 });
